46d2e5fd11217ae604c9b441539eff1cdc7457ca
Mauricio Pacheco <mauriciopachecoc@gmail.com>
2022-05-10 21:16:30 -0400

    Connect with api to post feedback

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -4,28 +4,39 @@ import { ArrowLeft } from "phosphor-react";
 import { FeedbackType, feedbackTypes } from ".."
 import { CloseButton } from "../../CloseButton"
 import { ScreenshotButton } from "../ScreenshotButton";
+import { api } from "../../../lib/api";
+import { Loading } from "../../Loading";
 
 interface FeedbackContentStepProps {
   feedbackType: FeedbackType;
   onFeedbackRestartRequested: () => void;
+  onFeedbackSent: () => void;
 }
 
 export function FeedbackContentStep({ 
   feedbackType, 
-  onFeedbackRestartRequested 
+  onFeedbackRestartRequested,
+  onFeedbackSent 
 }: FeedbackContentStepProps) {
   const [screenshot, setScreenshot] = useState<string | null>(null)
   const [comment, setComment] = useState('')
+  const [isSendingFeedback, setIsSendingFeedback] = useState(false)
 
   const feedbackTypeInfo = feedbackTypes[feedbackType]
 
-  function handleSubmitFeedback(event: FormEvent) {
+  async function handleSubmitFeedback(event: FormEvent) {
     event.preventDefault()
 
-    console.log({
-      screenshot,
-      comment
+    setIsSendingFeedback(true)
+
+    await api.post('/feedbacks', {
+      type: feedbackType,
+      comment,
+      screenshot
     })
+
+    setIsSendingFeedback(false)
+    onFeedbackSent()
   }
   
   return (
@@ -62,10 +73,10 @@ export function FeedbackContentStep({
 
           <button
             type="submit"
-            disabled={comment.length === 0}
+            disabled={comment.length === 0 || isSendingFeedback}
             className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
           >
-            Send Feedback
+            { isSendingFeedback ? <Loading /> : 'Send feedback' }
           </button>
         </footer>
       </form>
--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep"
 import { FeedbackContentStep } from "./Steps/FeedbackContentStep"
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep"
 
 import bugImageUrl from '../../assets/bug.svg'
 import ideaImageUrl from '../../assets/idea.svg'
@@ -35,22 +36,31 @@ export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+  const [feedbackSent, setFeedbackSent] = useState(false)
 
   function handleRestarFeedback() {
     setFeedbackType(null)
+    setFeedbackSent(false)
   }
 
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
 
-      {!feedbackType ? (
-       <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+      { feedbackSent ? (
+        <FeedbackSuccessStep onFeedbackRestartRequested={handleRestarFeedback} />
       ) : (
-        <FeedbackContentStep 
-          feedbackType={feedbackType}  
-          onFeedbackRestartRequested={handleRestarFeedback}
-        />
-      )} 
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep 
+              feedbackType={feedbackType}  
+              onFeedbackRestartRequested={handleRestarFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
+      ) } 
 
       <footer className="text-xs text-neutral-400">
         Made with ♥ by <a className="underline underline-offset-2" href="#">me</a>
